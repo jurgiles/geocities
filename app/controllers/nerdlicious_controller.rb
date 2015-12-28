19c62b2001764d15a0bc400709ab4fe69eb66727
Johny Urgiles <jurgiles@thoughtworks.com>
2015-12-28 00:28:18 -0500

    have questions change at 3 am

--- app/controllers/nerdlicious_controller.rb
+++ app/controllers/nerdlicious_controller.rb
@@ -2,7 +2,7 @@ class NerdliciousController < ApplicationController
   respond_to :html
 
   def show
-    @question = Question.where(index: Time.now.day).first()
+    @question = Question.where(index: (Time.now - 3.hours).day).first()
 
     unless @question
       @question = Question.new(index: -1, question: 'Whoops, questions went dark', answer: 'Missing In Action')
