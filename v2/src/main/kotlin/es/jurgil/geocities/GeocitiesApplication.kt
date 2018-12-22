33a8c33980d5a9cd398440ae8f495ea9452a903a
Johny Urgiles <jurgiles@Johnys-MBP.home>
2018-12-22 14:01:49 -0500

    add spring kotlin project in v2

---
+++ v2/src/main/kotlin/es/jurgil/geocities/GeocitiesApplication.kt
@@ -0,0 +1,12 @@
+package es.jurgil.geocities
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeocitiesApplication
+
+fun main(args: Array<String>) {
+	runApplication<GeocitiesApplication>(*args)
+}
+
